16201c865ee71f271d9a1a9531b888b6632452dc
Quentin REY <quentin.rey@etu.emse.fr>
2023-02-26 19:49:24 +0100

    add basic support of png images

--- Détourer et changer la couleur d'une image.py
+++ Détourer et changer la couleur d'une image.py
@@ -1,5 +1,6 @@
 from PIL import Image
-#import PySimpleGUI as sg
+import PySimpleGUI as sg
+import os.path
 
 
 def hex_to_rgb(value):
@@ -8,41 +9,135 @@ def hex_to_rgb(value):
     return list(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
 
-#sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
+def removeBackground(opacity, colorToRemove, colorToAdd, path, margin):
 
-colorToRemove = "ffffff"  # possible to specify using rgb in a list or in hexadecimal with "#00784d"
-colorToAdd = "ffffff"  # possible to specify using rgb in a list or in hexadecimal with "#00784d"
-opacity = 0
-margin = 250
+    windowsPath = path[2:]  # jpg ou png
+    # changer les / par des / avec ctrl+r
+
+    if type(colorToRemove) is str:
+        colorToRemove = hex_to_rgb(colorToRemove)
+
+    if type(colorToAdd) is str:
+        colorToAdd = hex_to_rgb(colorToAdd)
+
+    colorToAdd.append(int(opacity * 255))  # to add the opacity component to the rgb form
+
+    img = Image.open(windowsPath)
+
+    rgba = img.convert("RGBA")
+
+    datas = rgba.getdata()
+    newData = []
+
+    for image in datas:
+        if ((image[0] >= colorToRemove[0] - margin) and (image[0] <= colorToRemove[0] + margin) and (
+                image[1] >= colorToRemove[1] - margin) and (image[1] <= colorToRemove[1] + margin) and (
+                image[2] >= colorToRemove[2] - margin) and (image[2] <= colorToRemove[2] + margin)):
+            newData.append((colorToAdd[0], colorToAdd[1], colorToAdd[2], colorToAdd[3]))
+        else:
+            newData.append(image)
+
+    rgba.putdata(newData)
+
+    rgba.save(windowsPath[:-4] + "-modified.png", "PNG")
 
 
-windowsPath = "C:/Users/quent/Desktop/dégradé de médaille automne 2021.png"[2:]  # jpg ou png
-# changer les / par des / avec ctrl+r
+file_list_column = [
+    [
+        sg.Text("Image Folder"),
+        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+        sg.FolderBrowse(),
+    ],
+    [
+        sg.Listbox(
+            values=[], enable_events=True, size=(70, 30), key="-FILE LIST-"
+        )
+    ],
+    [
+        sg.Text("Chose opacity of the final replaced pixels"),
+        sg.Slider((0, 1), 0, 0.01, 0.5, 'horizontal', False, size=(20, 10), key="-OPACITY-", enable_events=True)
+    ],
+    [
+        sg.Input(key="-REMOVE_COLOR-"),
+        sg.ColorChooserButton("Removed color")
+    ],
+    [
+        sg.Input(key="-NEW_COLOR-"),
+        sg.ColorChooserButton("New color")
+    ],
+    [
+        sg.Slider((0, 250), 15, 1, 50, 'horizontal', False, key="-MARGIN-")
+    ],
+    [
+        sg.Button("Execute")
+    ]
+]
 
 
-if type(colorToRemove) is str:
-    colorToRemove = hex_to_rgb(colorToRemove)
+# For now will only show the name of the file that was chosen
+image_viewer_column = [
+    [sg.Image(key="-IMAGE-")]
+]
 
-if type(colorToAdd) is str:
-    colorToAdd = hex_to_rgb(colorToAdd)
 
-colorToAdd.append(int(opacity * 255))  # to add the opacity component to the rgb form
+# ----- Full layout -----
+layout = [
+    [
+        sg.Column(file_list_column),
+        sg.VSeperator(),
+        sg.Column(image_viewer_column),
+    ]
+]
 
-img = Image.open(windowsPath)
 
-rgba = img.convert("RGBA")
+window = sg.Window("Background remover", layout)
 
-datas = rgba.getdata()
-newData = []
 
-for image in datas:
-    if ((image[0] >= colorToRemove[0] - margin) and (image[0] <= colorToRemove[0] + margin) and (
-            image[1] >= colorToRemove[1] - margin) and (image[1] <= colorToRemove[1] + margin) and (
-            image[2] >= colorToRemove[2] - margin) and (image[2] <= colorToRemove[2] + margin)):
-        newData.append((colorToAdd[0], colorToAdd[1], colorToAdd[2], colorToAdd[3]))
-    else:
-        newData.append(image)
+# Run the Event Loop
+while True:
+    event, values = window.read()
 
-rgba.putdata(newData)
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
 
-rgba.save(windowsPath[:-4] + "-modified.png", "PNG")
+    # Folder name was filled in, make a list of files in the folder
+    if event == "-FOLDER-":
+        folder = values["-FOLDER-"]
+        print(folder)
+        try:
+            # Get list of files in folder
+            file_list = os.listdir(folder)
+        except:
+            file_list = []
+
+        # Remove all not wanted extension with a comprehension for
+        fnames = [
+            f
+            for f in file_list
+            if os.path.isfile(os.path.join(folder, f))
+            and f.lower().endswith((".png", ".gif", ".jpg", ".jpeg"))
+        ]
+        window["-FILE LIST-"].update(fnames)
+
+    elif event == "-FILE LIST-":  # A file was chosen from the listbox
+        try:
+            filename = os.path.join(
+                values["-FOLDER-"], values["-FILE LIST-"][0]
+            )
+
+            window["-IMAGE-"].update(filename=filename)
+        except:
+            pass
+
+    # call the function when push execute
+    elif event == "Execute":
+        removeBackground(values["-OPACITY-"],
+                         values["-REMOVE_COLOR-"],
+                         values["-NEW_COLOR-"],
+                         os.path.join(values["-FOLDER-"], values["-FILE LIST-"][0]),
+                         values["-MARGIN-"]
+                         )
+
+window.close()
+
+
